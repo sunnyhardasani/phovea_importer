c768c7249d586773f0c13c3068324c7c04af7c06
sunnyhardasani <sunny.hardasani@gmail.com>
2015-12-23 09:18:30 -0700

    gh-pages issue - resolved variable errors
    Signed-off-by: sunnyhardasani <sunny.hardasani@gmail.com>

--- bower_components/colorbrewer/colorbrewer.js
+++ bower_components/colorbrewer/colorbrewer.js
@@ -1,4 +1,5 @@
 // This product includes color specifications and designs developed by Cynthia Brewer (http://colorbrewer.org/).
+define(function(){
 var colorbrewer = {YlGn: {
 3: ["#f7fcb9","#addd8e","#31a354"],
 4: ["#ffffcc","#c2e699","#78c679","#238443"],
@@ -300,3 +301,5 @@ var colorbrewer = {YlGn: {
 11: ["#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5"],
 12: ["#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5","#ffed6f"]
 }};
+    return colorbrewer;
+});
