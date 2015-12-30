5c51f61677c6e35cea56d108e91e00c3dcd8e8cd
sunnyhardasani <sunny.hardasani@gmail.com>
2015-12-30 00:16:05 -0700

    issue#34 closed -  missing distinction between nominal and ordinal data type

--- js/table.js
+++ js/table.js
@@ -244,12 +244,10 @@ define(["jquery", "d3", "d3-tip",
                 else if ($(this).val() === DATATYPE_NUMERICAL) {
                     self.parentInstance.changeDataType(colId, DATATYPE_NUMERICAL);
                 }
-               /* else if ($(this).val() === DATATYPE_ORDINAL) {
-                 //self.parentInstance.changeDataType(colId,DATATYPE_ORDINAL);
-                 }*/
-
+                else if ($(this).val() === DATATYPE_ORDINAL) {
+                    self.parentInstance.changeDataType(colId,DATATYPE_ORDINAL);
+                }
                 $('#datatype-pop-up').hide();
-
             });
 
         }
@@ -314,6 +312,170 @@ define(["jquery", "d3", "d3-tip",
             var d3FreMap = d3.entries(freqMap);
             var index = 0;
 
+            //Set up the required x, y and
+            //color scales
+            //Reference : http://jsfiddle.net/59vLw/
+            var x = d3.scale.ordinal()
+                .rangeRoundBands([0, width], .1);
+            var y = d3.scale.linear()
+                .range([height, 0]);
+            var o = d3.scale.ordinal()
+                .domain(d3FreMap.map(function (d) {
+                    return d.key;
+                }))
+                .range(selColor);
+
+            //Initialize the d3 tip for displaying
+            //value for each bar on mouse hover
+            var tip = d3tip()
+                .attr('class', 'd3-tip')
+                .offset([-10, 0])
+                .html(function (d) {
+                    return d.key + " : " + d.value.value;
+                });
+
+            //Form new svg and attach d3 tip
+            var svg = d3.select(svgArea)
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            svg.call(tip);
+
+            //this will take the take got from the
+            //data wrnagler and sort the data accordingly
+            //to plot on the graph
+            d3FreMap.sort(function (a, b) {
+                if (a.value.sortIndex > b.value.sortIndex) {
+                    return 1;
+                }
+                else if (a.value.sortIndex < b.value.sortIndex) {
+                    return -1;
+                }
+                return 0;
+            })
+
+            // The following code was contained in the callback function.
+            x.domain(d3FreMap.map(function (d) {
+                return d.key;
+            }));
+            y.domain([0, d3.max(d3FreMap, function (d) {
+                return d.value.value;
+            })]);
+
+            // Attach all the data required by the D3
+            d3FreMap.map(function (d) {
+
+                d.x = x(d.key);
+                d.y = y(d.value.value);
+
+                //add the real freq object to further sort and take the data
+                d.freObjKey = d.key;
+                d.freObjValue = d.value;
+
+                // appending the svg area on the bar graph to fetch information for mouse event
+                d.index = index++;
+                d.svg = svgArea;
+                d.colId = col.id - 1;
+
+            });
+
+            self.d3FreMapArr[col.id - 1] = d3FreMap;
+
+            var bars = svg.selectAll(".bar")
+                .data(d3FreMap)
+                .enter().append("rect")
+                .attr("svg-info", function (d) {
+                    return d.svg;
+                }) // appending the svg area on the bar graph to fetch information for mouse event
+                .attr("class", "bar")
+                .attr("x", function (d) {
+                    return d.x;
+                })
+                .attr("width", x.rangeBand())
+                .attr("y", function (d) {
+                    return d.y-5;
+                })
+                .attr("height", function (d) {
+                    return (height - y(d.value.value)) < 10 ? 10 : 10 + (height - y(d.value.value));
+                })
+                .on('mouseover', tip.show)
+                .on('mouseout', tip.hide)
+                .style("fill", function (d) {
+
+                    //if color is null then set the
+                    //color and send otherwise send
+                    //the attached color
+                    if (d.freObjValue.color === "") {
+                        d.freObjValue.color = o(d.key);
+                    }
+
+                    return d.freObjValue.color;
+                })
+                .style("stroke-width","0.5")
+                .style("stroke","rgb(0,0,0)")
+                .on("contextmenu", function (d) {
+
+                    var selectedSVG = d.svg;
+                    var colId = d.colId;
+
+                    self.createColorBox("ordinalScale");
+
+                    self.colorBox.on("click", function (d) {
+
+                        var keys = Object.keys(colorbrewer["ordinalScale"][d.key]);
+                        var lastKey = keys[keys.length - 1];
+
+
+                        self.parentInstance.changeColColor(colId, colorbrewer["ordinalScale"][d.key][lastKey]);
+
+                        $('#colorbox-pop-up').hide();
+                    })
+
+                        //todo need to check the below code whether its required or not
+                        .selectAll(".swatch")
+                        .data(function (d) {
+                            return d.value[d3.keys(d.value).map(Number).sort(d3.descending)[0]];
+                        })
+                        .enter().append("div")
+                        .attr("class", "swatch")
+                        .style("background-color", function (d) {
+                            return d;
+                        });
+
+                    $('#colorbox-pop-up')
+                        .show()
+                        .css('top', d3.event.pageY)
+                        .css('left', d3.event.pageX)
+                        .appendTo('body');
+
+                    d3.event.preventDefault();
+                });
+
+            function type(d) {
+                d.value.value = +d.value.value;
+                return d;
+            }
+        }
+
+        Table.prototype.printOrdinalGraph  = function(_col, _svgArea){
+            var self = this;
+
+            //set up the margins of the svg
+            var margin = {top: 5, right: 5, bottom: 0, left: 5},
+                width = 150 - margin.left - margin.right,
+                height = 100 - margin.top - margin.bottom;
+
+            //take the col
+            var col = _col;
+            var svgArea = _svgArea;
+
+            //fetch required data
+            var selColor = col.colorScheme;
+            var dataTypeObj = col.dataTypeObj;
+            var dataType = dataTypeObj.type;
+            var freqMap = dataTypeObj.keyCountMap;
+            var keys = Object.keys(freqMap);
+            var d3FreMap = d3.entries(freqMap);
+            var index = 0;
+
             //Initialize the drag and drop behaviour on
             //the bar graph to drag and drop the bars for
             //nominal to oridinal conversion
@@ -344,8 +506,7 @@ define(["jquery", "d3", "d3-tip",
                 .attr('class', 'd3-tip')
                 .offset([-10, 0])
                 .html(function (d) {
-                    return "Freq[&nbsp;" + d.key
-                        + "&nbsp;]:  <span style='color:red'>" + d.value.value + "</span>";
+                    return d.key + " : " + d.value.value;
                 });
 
             //Form new svg and attach d3 tip
@@ -588,7 +749,7 @@ define(["jquery", "d3", "d3-tip",
                 .attr('class', 'd3-tip')
                 .offset([-10, 0])
                 .html(function (d) {
-                    return "<strong>Range: [</strong>" + d3.min(d) + " - " + d3.max(d) + "<strong>]</strong>";
+                    return "" + d3.min(d) + " - " + d3.max(d);
                 });
 
             //this will select the svg area to print the chart
@@ -753,12 +914,11 @@ define(["jquery", "d3", "d3-tip",
 
                         if (i == (histogram.length)) {
                             return "<strong>Invalid &nbsp; values &nbsp; frequency &nbsp; : " +
-                                "</strong> <span style='color:red'>" + d.y +
-                                "</span><strong></strong>";
+                                "</strong>" + d.y +
+                                "<strong></strong>";
                         }
                         else {
-                            return "<strong>Range: [</strong> <span style='color:red'>" +
-                                d3.min(d) + " - " + d3.max(d) + "</span><strong>]</strong>";
+                            return "" + d3.min(d) + " - " + d3.max(d);
                         }
                     });
 
@@ -841,12 +1001,10 @@ define(["jquery", "d3", "d3-tip",
                     .offset([-10, 0])
                     .html(function (d, i) {
                         if (i == (histogram.length - 1)) {
-                            return "<strong>Invalid &nbsp; values &nbsp; frequency" +
-                                " &nbsp; : </strong> <span style='color:red'>" + d.y +
-                                "</span><strong></strong>";
+                            return "<strong>Invalid &nbsp; value " + d.y +
+                                "</strong>";
                         } else {
-                            return "<strong>Range: [</strong> <span style='color:red'>" +
-                                d3.min(d) + " - " + d3.max(d) + "</span><strong>]</strong>";
+                            return "" + d3.min(d) + " - " + d3.max(d);
                         }
                     });
 
@@ -937,6 +1095,9 @@ define(["jquery", "d3", "d3-tip",
 
                 if (dataType == DATATYPE_NOMINAL) {
                     self.printNominalGraph(col,svgArea);
+                }
+                else if (dataType == DATATYPE_ORDINAL) {
+                    self.printOrdinalGraph(col,svgArea);
                 }
                 else if (dataType == DATATYPE_NUMERICAL) {
                     self.printNumericalGraph(col,svgArea);
@@ -1231,7 +1392,7 @@ define(["jquery", "d3", "d3-tip",
                     if(d.dataTypeObj.type == DATATYPE_NUMERICAL){
                         self.numericalOpr(d3.select(this),d);
                     }
-                    else if(d.dataTypeObj.type == DATATYPE_NOMINAL){
+                    else if(d.dataTypeObj.type == DATATYPE_NOMINAL || d.dataTypeObj.type == DATATYPE_ORDINAL){
                         self.nominalOpr(d3.select(this),d);
                     }
                 });
